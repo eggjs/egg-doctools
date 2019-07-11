b9ef815b1e65de59d217261599f6442ad2d3545f
Suyi <thonatos.yang@gmail.com>
2019-07-11 12:24:41 +0800

    feat: support watch source

--- command/dev.js
+++ command/dev.js
@@ -6,8 +6,12 @@ class DevCommand extends Command {
   async run(ctx) {
     await super.run(ctx);
 
+    // watch
+    this.watch(ctx.argv);
+
+    // run dev server
     const options = this.getConfig(ctx.argv);
-    return this.render('dev', options);
+    return this.render('dev', options, ctx.argv);
   }
 
   get description() {
