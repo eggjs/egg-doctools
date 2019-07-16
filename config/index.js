b402d08be83d2867a73b0365db970723a8135420
Suyi <thonatos.yang@gmail.com>
2019-07-16 18:50:11 +0800

    fix: update config

--- config/index.js
+++ config/index.js
@@ -5,7 +5,6 @@ module.exports = {
     title: 'Doctools For Egg.js',
   },
   theme: '@vuepress/theme-default',
-  // theme: '@eggjs/vuepress-theme-egg',
   markdown: {
     lineNumbers: true,
   },
